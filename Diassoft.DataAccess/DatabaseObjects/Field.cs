fe305d443b2ed0b7dc871aec7fb2a8dcce7d74d5
Olavo Henrique Dias - Diassoft <diassoft@icloud.com>
2017-09-04 15:55:10 -0600

    Created the Field Class

--- Diassoft.DataAccess/DatabaseObjects/Field.cs
+++ Diassoft.DataAccess/DatabaseObjects/Field.cs
@@ -4,13 +4,40 @@ using System.Text;
 
 namespace Diassoft.DataAccess.DatabaseObjects
 {
-    public class Field
+    /// <summary>
+    /// Represents the base for any Field
+    /// </summary>
+    public abstract class Field
     {
-
+        /// <summary>
+        /// The Field Name
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The Field Type according to the <see cref="FieldTypes"/>.
+        /// </summary>
         public FieldTypes Type { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of a <see cref="Field"/>.
+        /// </summary>
+        public Field()
+        {
+            Name = "";
+            Type = FieldTypes.None;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of a <see cref="Field"/>.
+        /// </summary>
+        /// <param name="name">The Field Name</param>
+        /// <param name="type">The Field Type according to the <see cref="FieldTypes"/> enumeration.</param>
+        public Field(string name, FieldTypes type)
+        {
+            Name = name;
+            Type = type;
+        }
     }
 
 }
