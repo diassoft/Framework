a448ab827a6356a0e6917b2c260bff8285a81136
Olavo Henrique Dias - Diassoft <diassoft@icloud.com>
2017-09-04 22:05:40 -0600

    Added Unit Testing for Diassoft.DataAccess

---
+++ Diassoft.DataAccess.Tests/DatabaseOperationsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Diassoft.DataAccess.Tests
+{
+    [TestClass]
+    public class DatabaseOperationsTest
+    {
+        [TestMethod]
+        public void TestSelectDbOperation_001_SimpleSelect()
+        {
+        }
+    }
+}
---
+++ Diassoft.DataAccess.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Diassoft.DataAccess.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Diassoft.DataAccess.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e62f86e9-442a-4c45-96ed-6c987b7c669e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
