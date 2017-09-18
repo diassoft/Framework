fcda20b1581dcb035b270f8a7e81d0986442633e
Olavo Henrique Dias - Diassoft <diassoft@icloud.com>
2017-09-18 00:32:43 -0600

    Added the DateTimeExtensions Methods ToJulian and CreateFromJulian.

---
+++ Diassoft.Intelligence.Tests/TestJulianDateExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Diassoft.Intelligence.Extensions;
+
+
+namespace Diassoft.Intelligence.Tests
+{
+    [TestClass]
+    public class TestJulianDateExtensions
+    {
+        [TestMethod]
+        public void TestToJulian_001()
+        {            
+            Assert.AreEqual<int>(110001, 
+                                 new DateTime(2010, 1, 1).ToJulian());
+
+            Assert.AreEqual<int>(117021,
+                                 new DateTime(2017, 1, 21).ToJulian());
+
+        }
+
+        [TestMethod]
+        public void TestFromJulian_002()
+        {
+            Assert.AreEqual<DateTime>(new DateTime(2010, 1, 1), new DateTime().CreateFromJulian(110001));
+            Assert.AreEqual<DateTime>(new DateTime(2017, 1, 21), new DateTime().CreateFromJulian(117021));
+
+        }
+    }
+}
---
+++ Diassoft.Intelligence/Exceptions/InvalidJulianDateException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diassoft.Intelligence.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when the <see cref="JulianDate">Julian Date</see> is invalid.
+    /// </summary>
+    public class InvalidJulianDateException : System.Exception
+    {
+        /// <summary>
+        /// The Julian Date
+        /// </summary>
+        public int JulianDate { get; set; }
+
+        /// <summary>
+        /// The Gregorian Date
+        /// </summary>
+        public DateTime GregorianDate { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidJulianDateException"/>.
+        /// </summary>
+        /// <param name="julianDate">The Julian Date</param>
+        public InvalidJulianDateException(int julianDate) : base($"{julianDate} is an invalid Julian Date. It should be a value between 1 and 999365")
+        {
+            JulianDate = julianDate;
+            GregorianDate = DateTime.MinValue;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidJulianDateException"/>.
+        /// </summary>
+        /// <param name="gregorianDate">The gregorian date</param>
+        public InvalidJulianDateException(DateTime gregorianDate) : base($"{gregorianDate.ToString("yyyy-MM-dd")} is an invalid Gregorian Date. Year should not be less than 1900.")
+        {
+            JulianDate = 0;
+            GregorianDate = gregorianDate;
+        }
+
+    }
+}
