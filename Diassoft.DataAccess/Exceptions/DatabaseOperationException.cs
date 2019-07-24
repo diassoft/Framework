c3d4bee7490b7bb6104ceb96664fd9a9b4c67c71
Olavo Henrique Dias - Diassoft <diassoft@icloud.com>
2019-07-24 00:55:00 -0600

    Added new exceptions for better error handling.

---
+++ Diassoft.DataAccess/Exceptions/DatabaseOperationException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diassoft.DataAccess.Exceptions
+{
+    /// <summary>
+    /// Represents an Exception thrown when there is an error in a database operation
+    /// </summary>
+    public class DatabaseOperationException: System.Exception
+    {
+        /// <summary>
+        /// The Sql Statement
+        /// </summary>
+        public string SqlStatement { get; protected set; }
+
+        /// <summary>
+        /// Initializes a new instance of the DatabaseOperationException
+        /// </summary>
+        public DatabaseOperationException(): this("Error during the execution of a database operation") { }
+
+        /// <summary>
+        /// Initializes a new instance of the DatabaseOperationException
+        /// </summary>
+        /// <param name="message">The Error Message</param>
+        public DatabaseOperationException(string message): this(message, null) { }
+
+        /// <summary>
+        /// Initializes a new instance of the DatabaseOperationException
+        /// </summary>
+        /// <param name="message">The Error Message</param>
+        /// <param name="innerException">The inner exception to complement the exception</param>
+        public DatabaseOperationException(string message, Exception innerException): this(message, innerException, null) { }
+
+        /// <summary>
+        /// Initializes a new instance of the DatabaseOperationException
+        /// </summary>
+        /// <param name="message">The Error Message</param>
+        /// <param name="innerException">The inner exception to complement the exception</param>
+        /// <param name="sqlStatement">The SQL Statement generating the failure</param>
+        public DatabaseOperationException(string message, Exception innerException, string sqlStatement): base(message, innerException)
+        {
+            SqlStatement = sqlStatement;
+        }
+
+    }
+}
---
+++ Diassoft.DataAccess/Exceptions/PrimaryKeyViolationException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diassoft.DataAccess.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when a Primary Key is violated
+    /// </summary>
+    public class PrimaryKeyViolationException : DatabaseOperationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the Primary Key Violation Exception
+        /// </summary>
+        public PrimaryKeyViolationException(): this("A primary key violation prevented the operation from happening") { }
+        /// <summary>
+        /// Initializes a new instance of the Primary Key Violation Exception
+        /// </summary>
+        /// <param name="message">The Error Message</param>
+        public PrimaryKeyViolationException(string message) : this(message, null) { }
+        /// <summary>
+        /// Initializes a new instance of the Primary Key Violation Exception
+        /// </summary>
+        /// <param name="message">The Error Message</param>
+        /// <param name="innerException">The inner exception to complement the exception</param>
+        public PrimaryKeyViolationException(string message, Exception innerException): this(message, innerException, null) { }
+        /// <summary>
+        /// Initializes a new instance of the Primary Key Violation Exception
+        /// </summary>
+        /// <param name="message">The Error Message</param>
+        /// <param name="innerException">The inner exception to complement the exception</param>
+        /// <param name="sqlStatement">The SQL Statement generating the failure</param>
+        public PrimaryKeyViolationException(string message, Exception innerException, string sqlStatement) : base(message, innerException, sqlStatement) { }
+    }
+}
